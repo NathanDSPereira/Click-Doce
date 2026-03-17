31d1c3747dd64de1bd70185553e9a1c91a4fc99f
Nathan da Silva Pereira <nathan.pereira@unimeduberaba.com.br>
2026-03-17 17:43:30 -0300

    ajustando o layout do cardCarrinho

--- components/CarrinhoCard.tsx
+++ components/CarrinhoCard.tsx
@@ -5,9 +5,10 @@ import { Minus, Plus, Trash2 } from "lucide-react";
 
 import Image from "next/image";
 
-export default function CarrinhoCard({item, aumentarQuantidade, diminuirQuantidade, removerItem}: {item: CarrinhoItem, aumentarQuantidade: () => void, diminuirQuantidade: () => void, removerItem: () => void}) {
-
-
+export default function CarrinhoCard({item, aumentarQuantidade, diminuirQuantidade,
+removerItem
+}: { item: CarrinhoItem, aumentarQuantidade: () => void, diminuirQuantidade: () => void, removerItem: () => void
+}) {
     return (
         <div className="flex gap-4 p-2 rounded-2xl border border-(--text-chocolate) shadow-md w-11/12 md:w-10/12 ">
             <div className="relative w-24 h-24 shrink-0">
@@ -25,15 +26,12 @@ export default function CarrinhoCard({item, aumentarQuantidade, diminuirQuantida
                     <h3 className="font-bold text-(--text-chocolate) text-base uppercase">
                         {item.nome}
                     </h3>
-                    
+                    <p>
+                        {formatarMoeda(item.preco)} cada
+                    </p>
                 </div>
 
                 <div className="flex items-end justify-between mt-4">
-                    <div>
-                        <p className="text-(--accent-green-dark) font-semibold text-sm">
-                            {item.quantidade}x - {formatarMoeda(item.preco * item.quantidade)}
-                        </p>
-                    </div>
 
                     <div className="flex items-center gap-3">
                         <button onClick={diminuirQuantidade} className="text-(--border-creme) bg-(--text-chocolate) border rounded-xl w-9 h-9 flex justify-center items-center">
@@ -43,6 +41,12 @@ export default function CarrinhoCard({item, aumentarQuantidade, diminuirQuantida
                         <button onClick={aumentarQuantidade} className="text-(--border-creme) bg-(--text-chocolate) border rounded-xl w-9 h-9 flex justify-center items-center">
                             <Plus size={20}/>
                         </button>
+                    </div>
+                    
+                    <div>
+                        <p className="text-(--accent-green-dark) text-sm">
+                            {item.quantidade}x - {formatarMoeda(item.preco * item.quantidade)}
+                        </p>
                     </div>
                 </div>
             </div>
